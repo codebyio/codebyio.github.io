cd4a090177fe6e789ea4dd67313887359c4cc587
Indra Oei <indraoei25@gmail.com>
2025-03-10 21:27:41 +0700

    UI: Window Card Label

--- src/components/shared/WindowCard/WindowCard.jsx
+++ src/components/shared/WindowCard/WindowCard.jsx
@@ -1,15 +1,24 @@
 import classes from "./WindowCard.module.css";
+import { PiTreeStructure } from "react-icons/pi";
 
 const WindowCard = ({ title, body, invertColor }) => {
   return (
     <article className={`${classes["window-card"]} ${invertColor && classes["inverted"]}`}>
       <div className={classes["window-card__tool"]}>
-        <span className={classes["min"]}></span>
-        <span className={classes["max"]}></span>
-        <span className={classes["close"]}>
-          <span></span>
-          <span></span>
-        </span>
+        <div>
+          <p className={classes["window-card__tool-label"]}>
+            <PiTreeStructure fontSize='1.15rem' />
+            System
+          </p>
+        </div>
+        <div>
+          <span className={classes["min"]}></span>
+          <span className={classes["max"]}></span>
+          <span className={classes["close"]}>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
       </div>
       <div className={classes["window-card__content"]}>
         <h2 className={classes["window-card__title"]}>{title}</h2>
