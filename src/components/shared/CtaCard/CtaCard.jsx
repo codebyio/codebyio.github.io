1d47e098ff7c06a008e584ae4d49f76d38b0388a
Indra Oei <indraoei25@gmail.com>
2025-06-24 14:52:22 +0700

    UI: add button to CtaCard for better UX

--- src/components/shared/CtaCard/CtaCard.jsx
+++ src/components/shared/CtaCard/CtaCard.jsx
@@ -7,6 +7,7 @@ const CtaCard = ({ title, subtitle, icon, invertColor }) => {
         <div className={classes["cta-card__icon-wrapper"]}>{icon}</div>
         <h2 className={classes["cta-card__title"]}>{title}</h2>
         <p className={classes["cta-card__subtitle"]}>{subtitle}</p>
+        <button className={classes["cta-card__button"]}>Learn More</button>
       </a>
     </article>
   );
